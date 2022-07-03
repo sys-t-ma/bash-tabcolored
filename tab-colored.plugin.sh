85115b2e2065a68f28a39bbcca8bacafabb0a42d
leofdss <leonardo.f.desouzasilva@gmail.com>
2022-07-03 11:54:59 -0300

    tabcolored

---
+++ tab-colored.plugin.sh
@@ -0,0 +1,7 @@
+#! bash oh-my-bash.module
+
+bind 'set show-all-if-ambiguous on'
+bind 'set menu-complete-display-prefix on'
+bind 'TAB: menu-complete'
+bind 'set colored-completion-prefix on'
+bind 'set colored-stats on'
